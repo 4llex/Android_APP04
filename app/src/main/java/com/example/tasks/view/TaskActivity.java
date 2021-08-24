31505011bf35777a44d3d52bf8c0fbf0b302bcc3
Alex Rosa <alexr@gec.inatel.br>
2021-08-23 21:27:14 -0300

    Layout - cadastro de tarefas

--- app/src/main/java/com/example/tasks/view/TaskActivity.java
+++ app/src/main/java/com/example/tasks/view/TaskActivity.java
@@ -1,9 +1,16 @@
 package com.example.tasks.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.tasks.R;
 import com.example.tasks.viewmodel.TaskViewModel;
@@ -11,7 +18,7 @@ import com.example.tasks.viewmodel.TaskViewModel;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
-public class TaskActivity extends AppCompatActivity {
+public class TaskActivity extends AppCompatActivity implements View.OnClickListener {
 
     private SimpleDateFormat mFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
     private ViewHolder mViewHolder = new ViewHolder();
@@ -27,11 +34,42 @@ public class TaskActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
+        this.mViewHolder.editDescription = findViewById(R.id.edit_description);
+        this.mViewHolder.spinnerPriority = findViewById(R.id.spinner_priority);
+        this.mViewHolder.checkComplete = findViewById(R.id.check_completed);
+        this.mViewHolder.buttonDate = findViewById(R.id.button_date);
+        this.mViewHolder.buttonSave = findViewById(R.id.button_save);
+
         // ViewModel
         this.mViewModel = new ViewModelProvider(this).get(TaskViewModel.class);
 
+        this.createEvents();
+
         // Cria observadores
         this.loadObservers();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.button_date) {
+
+        } else if (id == R.id.button_save) {
+
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void createEvents() {
+        this.mViewHolder.buttonSave.setOnClickListener(this);
+        this.mViewHolder.buttonDate.setOnClickListener(this);
     }
 
     /**
@@ -40,9 +78,18 @@ public class TaskActivity extends AppCompatActivity {
     private void loadObservers() {
     }
 
+
+
     /**
      * ViewHolder
      */
     private static class ViewHolder {
+
+        EditText editDescription;
+        Spinner spinnerPriority;
+        CheckBox checkComplete;
+        Button buttonDate;
+        Button buttonSave;
+
     }
 }
